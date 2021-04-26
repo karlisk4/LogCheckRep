2953f40a9363fb8477835fdf4ae8155c0ad9c335
Kārlis Skirmanis <Karlis.Skirmanis@edu.rtu.lv>
2021-04-27 01:08:03 +0300

    fixed 1 bug, 3 new ones :)

--- LogChecker/LogChecker.cpp
+++ LogChecker/LogChecker.cpp
@@ -25,18 +25,18 @@ int main()
     std::vector<std::string> regLst{};
     std::vector<std::string> drsLst{};
 
-    ifstream RegFile("C:\\Users\\karli\\Documents\\reg.txt");
+    ifstream RegFile("C:\\Users\\karlis\\Documents\\reg.txt");
 
     while (getline(RegFile, regLine)) {
         if (regLine.find(regTrg) != string::npos) {
 
             unsigned first = findLastIndex(regLine, ':');
             unsigned last = regLine.length()-5;
-            string rgName = regLine.substr(first + 2, last - first);
+            string rgName = regLine.substr(first + 2, last - first-1);
             regLst.push_back(rgName);
         }
     }
-    ifstream ReadFile("C:\\Users\\karli\\Documents\\output_2021-04-24_19-13-47.log");
+    ifstream ReadFile("C:\\Users\\karlis\\Documents\\output_2021-04-24_19-13-47.log");
 
     // Use a while loop together with the getline() function to read the file line by line
     while (getline(ReadFile, curLine)) {
@@ -63,26 +63,33 @@ int main()
         found = false;
         //cout << "First ";
         //cout << drsLst.at(0);
-        cout << "\n";
+        //cout << "\n";
         //cout << regLst.at(5);
-        int lstI = i;
         for (int j = 0; j < drsLst.size(); j++) {
-            cout << drsLst.at(j);
             string drsD = drsLst.at(j);
             string regD = regLst.at(i);
+            
+            /*string drsD = drsLst.at(0);
+            //cout << drsLst.at(j);
+            cout << "drs \n";
+            string regD = regLst.at(5);
+            cout << drsD.length();
+            cout << " reg \n";
+           cout << regD.length();*/
             if (drsD == regD) {
                 found = true;
-                cout << "Match!";
-                break;
+                i++;
+                //cout << "Match!";
             }
         }
         string name = regLst.at(i);
         if (found == false) {
             //cout << "Naughty!";
-            //cout << name;
+            cout << name;
+            cout << "drs \n";
             i++;
         }
-        cout << "\n";
+        //cout << "\n";
     }
 
     // Close the file
