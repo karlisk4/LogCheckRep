c6bdc23cd1f03082625d2899b714f20370c047f7
Kārlis Skirmanis <Karlis.Skirmanis@gmail.com>
2021-04-29 01:11:23 +0300

    Redid app detection output, fixed UNSERVED bug

--- LogChecker/LogChecker.cpp
+++ LogChecker/LogChecker.cpp
@@ -19,13 +19,15 @@ int main()
 {   
     string drsPen = "ACRLRD_DRS: Penalty";
     string drsRun = "ACRLRD_DRS is running";
+    string pstRac = "UNSERVED";
     string regTrg = ":flag";
     string regLine;
     string curLine;
     std::vector<std::string> regLst{};
     std::vector<std::string> drsLst{};
+    std::vector<std::string> delLst{};
 
-    ifstream RegFile("C:\\Users\\karlis\\Documents\\reg.txt");
+    ifstream RegFile("C:\\Users\\karli\\Documents\\reg.txt");
 
     while (getline(RegFile, regLine)) {
         if (regLine.find(regTrg) != string::npos) {
@@ -34,9 +36,10 @@ int main()
             unsigned last = regLine.length()-5;
             string rgName = regLine.substr(first + 2, last - first-1);
             regLst.push_back(rgName);
+            delLst.push_back(rgName);
         }
     }
-    ifstream ReadFile("C:\\Users\\karlis\\Documents\\output_2021-04-24_19-13-47.log");
+    ifstream ReadFile("C:\\Users\\karli\\Documents\\output_2021-04-24_19-13-47.log");
 
     // Use a while loop together with the getline() function to read the file line by line
     while (getline(ReadFile, curLine)) {
@@ -50,46 +53,31 @@ int main()
             //cout << "\n";
         }
         if (curLine.find(drsPen) != string::npos) {
-            unsigned first = curLine.find("(");
-            unsigned last = curLine.find(")");
-            string rnName = curLine.substr(first + 1, last - first - 1);
-            cout << rnName;
-            cout << " pen \n";
+            if (curLine.find(pstRac) != string::npos) {
+
+            } else {
+                unsigned first = curLine.find("(");
+                unsigned last = curLine.find(")");
+                string rnName = curLine.substr(first + 1, last - first - 1);
+                cout << rnName;
+                cout << " Illegal DRS penalty \n";
+            }
         }
         
     }
-    bool found;
-    for (int i = 0; i < regLst.size(); i) {
-        found = false;
-        //cout << "First ";
-        //cout << drsLst.at(0);
-        //cout << "\n";
-        //cout << regLst.at(5);
+    for (int i = 0; i < regLst.size(); i++) {
         for (int j = 0; j < drsLst.size(); j++) {
-            string drsD = drsLst.at(j);
-            string regD = regLst.at(i);
-            
-            /*string drsD = drsLst.at(0);
-            //cout << drsLst.at(j);
-            cout << "drs \n";
-            string regD = regLst.at(5);
-            cout << drsD.length();
-            cout << " reg \n";
-           cout << regD.length();*/
-            if (drsD == regD) {
-                found = true;
-                i++;
-                //cout << "Match!";
+            if (regLst.at(i)==drsLst.at(j)) {
+                delLst.at(i) = "";
             }
         }
-        string name = regLst.at(i);
-        if (found == false) {
-            //cout << "Naughty!";
-            cout << name;
-            cout << "drs \n";
-            i++;
+    }
+    for (int i = 0; i < delLst.size(); i++) {
+        if (delLst.at(i) != "") {
+            cout << delLst.at(i);
+            cout << " DRS app not detected \n";
         }
-        //cout << "\n";
+        
     }
 
     // Close the file
